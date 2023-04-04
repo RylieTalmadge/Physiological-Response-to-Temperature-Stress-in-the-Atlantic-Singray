24560c51c7de792e73241468da2ed593567e452b
Rylie Talmadge <talmadgere@g.cofc.edu>
2023-04-03 21:45:04 -0400

    edit code

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -5,91 +5,82 @@ library(ggplot2)
 resp_data <- read_excel('data/MetabolicCalc.xlsx', sheet = 'Processed Data', 
                   n_max = 4)
 
-# transpose a matrix
-?t() 
+# transpose matrix 
+t_resp_data <- t(resp_data)
+head(t_resp_data)
+
 tidyr::pivot_longer()
 
 # HSC= MO2 (metabolic rate formula)
-# y = K * V*B/M
-# y = MO2, K = Slope, V = volume of respirometer - volume of ray, B = constant, M = mass of ray (kg)
-# volume (V) is negligible (drop from equation)
+# y = K * V*B/M (y = MO2, K = Slope, V = volume of respirometer - volume of ray, B = constant, M = mass of ray (kg))
+# Ignore this equation for now, Use later (calculated by hand but will change in the next couple of days when we volume correct the equation)
+# The data entered here has those mock calculations done to be sure the rest of the analysis works
 calc_HSC <- function(K, B, M){
   K*B/M
 }
 
 with(resp_data, calc_HSC(Mass, B, Slope))
 
-# HSC Mean and Standard Deviation
-mean(my_data$metabolic_rate)
 
-# HSC Male Mean and Standard Deviation
-mean(my_data$metabolic_rate[my_data$gender == "male"])
+# HSC Male Mean
+HSC_male_mean <- mean(resp_data$metabolic_rate[resp_data$sex == "male"])
 
 # HSC Female Mean and Standard Deviation
-mean(my_data$metabolic_rate[my_data$gender == "female"])
+HSC_female_mean <- mean(resp_data$metabolic_rate[resp_data$sex == "female"])
 
 # HSC 16 Temp Mean and Standard Deviation
-mean(my_data$metabolic_rate[my_data$temperature == 16])
+HSC_16_mean <- mean(resp_data$metabolic_rate[resp_data$temperature == 16])
 
 # HSC 21 Temp Mean and Standard Deviation
-mean(my_data$metabolic_rate[my_data$temperature == 21])
+HSC_21_mean <- mean(resp_data$metabolic_rate[resp_data$temperature == 21])
 
 # HSC 26 Temp Mean and Standard Deviation
-mean(my_data$metabolic_rate[my_data$temperature == 26])
+HSC_26_mean <- mean(resp_data$metabolic_rate[resp_data$temperature == 26])
 
 # Index of Thermal Sensitivity (Q10)
 # Q10 = (K2/K1)^(10/t2-t1)
+# Ignore for now (may not include, depending on volume issue from above)
 calc_Q10 <- function(K1, K2, t1, t2){
   (K2/K1)^(10/(t2-t1))
 }
-# NOTE: change to fit data table
+
 with(resp_data, calc_Q10(Mass, B, Slope))
 
 # assign variables from data frame
-temperature <- resp_data$Temperature
-metabolic_rate <- resp_data$Metabolic_Rate
-sex <- resp_data$Sex
+Temperature <- resp_data$Temperature
+Metabolic_rate <- resp_data$Metabolic_Rate
+Sex <- resp_data$Sex
 
 # Create a scatterplot
-ggplot(resp_data, aes(x = temperature, y = metabolic_rate, color = sex)) +
+ggplot(resp_data, aes(x = Temperature, y = Metabolic_rate, color = Sex)) +
   geom_point()
 
-# Linear Regression Models
-resp_female <- lm(metabolic_rate ~ temperature, data = subset(resp_data, sex == "F"))
-summary(resp_female)
-plot(resp_female)
+# Box plot
+ggplot(resp_data, aes(x = factor(temperature), y = Metabolic_rate)) +
+  geom_boxplot() +
+  labs(x = "Temperature", y = "Metabolic Rate") +
+  ggtitle("Metabolic Rates by Temperature")
 
-resp_male <- lm(metabolic_rate ~ temperature, data = subset(resp_data, sex == "M"))
-summary(resp_male)
-plot(resp_male)
-
-# visualize the linear regression line on the scatter plot
-ggplot(data, aes(x = temperature, y = metabolic_rate, color = sex)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
+# Bar graph
+# mean metabolic rates by temperature
+mean_rates <- aggregate(metabolic_rate ~ temperature, data = resp_data, mean)
+# Create the bar graph
+ggplot(mean_rates, aes(x = Sex, y = Metabolic_rate, fill = Sex)) +
+  geom_col() +
+  labs(x = "Gender", y = "Mean Metabolic Rate") +
+  ggtitle("Mean Metabolic Rates by Gender")
 
 # Normal Data
 # Data were tested for normality and non-normal data were rank transformed.
+# Ignore for now until data issue is corrected (see volume issue above)
 shapiro.test(x)
 x_rank <- rank(x)
 
 # ANOVA Analysis
-# input data
-temperature <- c(20, 20, 20, 25, 25, 25, 30, 30, 30)
-metabolic_rate <- c(10, 12, 15, 18, 20, 22, 24, 26, 28)
-
-# create data frame
-data <- data.frame(temperature, metabolic_rate)
-
-# perform ANOVA
-model <- aov(metabolic_rate ~ temperature, data = data)
-
-# summarize ANOVA results
+model <- aov(metabolic_rate ~ temperature, data = resp_data)
 summary(model)
 
 # Two-way ANOVA to compare sex
-anova_model <- aov(Metabolic_Rate ~ Gender + Temperature + Gender:Temperature, data = data)
-
-# Check ANOVA results
+anova_model <- aov(Metabolic_Rate ~ Gender + Temperature + Sex:Temperature, data = resp_data)
 summary(anova_model)
 
